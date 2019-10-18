1a34cd938b72acd27e4a76da122bbdb152a54986
jsf5077 <jsf5077@gmail.com>
2019-10-17 21:52:01 -0400

    more projects added and changed their format. more minor styling changes.

--- js/init.js
+++ js/init.js
@@ -8,12 +8,14 @@
 })(jQuery); // end of jQuery name space
 
 
-var Jeff_Froehlich
 
-Jeff_Froehlich = {
-  Profession: "Full Stack Web Developer",
-  Website: https://jsf5077.github.io/Portfolio/,
-  Email: jsf5077@gmail.com,
-  Phone: 267-626-3019
-
+function contactMe() {
+  const Jeff_Froehlich = {
+    Role: "Full Stack Web Developer",
+    Phone: 267-626-3019,
+    Email: "jsf5077@gmail.com"
+  }
+  
 }
+
+contactMe();
