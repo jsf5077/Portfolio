fae9ac73dedd5efe0f7ac57514d8675f11e641c5
jsf5077 <jsf5077@gmail.com>
2019-10-23 19:57:14 -0400

    More styling updates to sections of the portfolio. Added a border around the projects to give them some spereation.

--- js/init.js
+++ js/init.js
@@ -8,14 +8,3 @@
 })(jQuery); // end of jQuery name space
 
 
-
-function contactMe() {
-  const Jeff_Froehlich = {
-    Role: "Full Stack Web Developer",
-    Phone: 267-626-3019,
-    Email: "jsf5077@gmail.com"
-  }
-  
-}
-
-contactMe();
